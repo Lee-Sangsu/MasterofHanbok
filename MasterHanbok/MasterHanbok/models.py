945673976eeb4bd04aaa2b368a04a26029d25877
fogbar <kimdongjun6570@gmail.com>
2020-07-06 18:41:06 +0900

    dd

--- MasterHanbok/MasterHanbok/models.py
+++ MasterHanbok/MasterHanbok/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
