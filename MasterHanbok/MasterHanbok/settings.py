516f1c5f19e7e3217ce6fe05556021b0b821a113
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-07-22 23:17:38 +0900

    settings 좀 고침

--- MasterHanbok/MasterHanbok/settings.py
+++ MasterHanbok/MasterHanbok/settings.py
@@ -155,4 +155,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# ROOT_DIR = os.path.dirname(BASE_DIR)
+
+# # static files
+# STATIC_URL = '/static/'
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+# STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
