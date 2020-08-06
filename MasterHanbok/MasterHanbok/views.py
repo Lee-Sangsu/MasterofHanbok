664072eba08124bcb367f035230a5f2292c9cbb0
Ubuntu <ubuntu@ip-172-31-36-225.ap-northeast-2.compute.internal>
2020-08-06 07:20:50 +0000

    되게 요구사항이 많구만'
    ;
    exit
    q
    .
    ?

--- MasterHanbok/MasterHanbok/views.py
+++ MasterHanbok/MasterHanbok/views.py
@@ -62,7 +62,7 @@ class UserLoginAPIView(ObtainJSONWebToken):
                         {'id': user.id}, SECRET_KEY, algorithm="HS256").decode('utf-8')
                     nickname = user.nickname
 
-                    return JsonResponse({"token": token, "nickname": nickname, "user_id": user.pk}, status=200)
+                    return JsonResponse({"token": token, "nickname": nickname, "user_pk": user.pk}, status=200)
 
                 else:
 
