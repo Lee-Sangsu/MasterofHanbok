edbf4c4fbd573adfce8f66a93171029e0c844ece
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-09-29 12:59:15 +0900

    tuple 이러면 죽여버린다

--- MasterHanbok/request/views.py
+++ MasterHanbok/request/views.py
@@ -136,7 +136,7 @@ class Biddings(View):
             bidding.save()
 
             devices = APNSDevice.objects.all()
-            return Response(devices.send_message("응답견적이 도착했습니다", badge=0, sound="default"), status=200)
+            return HttpResponse(devices.send_message("응답견적이 도착했습니다", badge=0, sound="default"), status=200)
             # else:
             #     return NotificationError(Exception)
         else:
