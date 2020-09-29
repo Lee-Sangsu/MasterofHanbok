1bbe27651483b53a656fe13ebe512c8a8acb7d4d
Lee-Sangsu <63631278+Lee-Sangsu@users.noreply.github.com>
2020-09-29 18:09:37 +0900

    역참조를 몇 번을 하는 것인가..

--- MasterHanbok/request/views.py
+++ MasterHanbok/request/views.py
@@ -135,7 +135,7 @@ class Biddings(View):
             )
             bidding.save()
 
-            apns_token = request.requested_user.push_notifications_apnsdevice.registration_id
+            apns_token = request.requested_user.push_notifications_apnsdevice_set.registration_id
 
             devices = APNSDevice.objects.get(registration_id=apns_token)
             return HttpResponse(devices.send_message("응답견적이 도착했습니다", badge=0, sound="default"), status=200)
