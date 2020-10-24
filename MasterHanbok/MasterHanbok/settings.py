d36ac269bd66627b9b90f506e3b93807e1cd7b46
Sangsu Lee <63631278+Lee-Sangsu@users.noreply.github.com>
2020-10-24 11:25:41 +0900

    hide secret key

--- MasterHanbok/MasterHanbok/settings.py
+++ MasterHanbok/MasterHanbok/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y4tvwoz_no60*3#g83-e)pyg*_8f111qktk6j3i&-66y3wy731'
+SECRET_KEY = '' # hide it for security
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
